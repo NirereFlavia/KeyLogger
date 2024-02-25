5c4ca2a6afc51d58087f275fd7aa22e2c810a50a
NirereFlavia <95276067+NirereFlavia@users.noreply.github.com>
2024-02-25 23:47:07 +0200

    Add files via upload
    Let's study kelogger together

---
+++ introKeylogger.py
@@ -0,0 +1,11 @@
+#A keylogger program is a program or a tool that is designed to secretly record the data that we type on a keyboard. Keylogger program is installed secretly on our system,
+# It targets the system when the user is working on it and records the keystrokes in a text file, the keylogger sends the text files to the cyber criminal, the recorded data can be
+# used by the hacker.
+
+#Some indicators of a keylogger in your system
+#1. Increased CPU usage, especially when you're not actively using the computer, may indicate the presence of a keylogger.
+#2. If your system behaves unusually, such as slow performance, freezing, or crashing, it may be a sign of malicious activity.
+#3. Some keyloggers may install additional toolbars or icons on your desktop without your knowledge.
+#4. Keyloggers may be associated with adware or malicious software that generates unwanted pop-ups or ads.
+#5. If you are using a laptop, an unexpected battery drain may be a sign of increased activity caused by a keylogger.
+#6. Keyloggers may send the recorded data over the Internet. Monitoring network activity for unexpected or unexplained data transfers can be a clue.
